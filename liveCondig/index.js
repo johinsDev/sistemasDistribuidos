8f705b1c254be42007686eedbcbd12ed93b3d9fe
Johan Villamil <johan.villamil@mocionsoft.com>
2017-04-07 16:29:24 -0500

    CSV

--- liveCondig/index.js
+++ liveCondig/index.js
@@ -3,20 +3,20 @@ var Promise = require('promise');
 
 var http = require('http');
 var rest = require('restler');
+var fs = require('fs');
+var csv = require('fast-csv');
+var ws = fs.createWriteStream('my.csv')
 
-console.log("longitude,latitude");
 
-meli.search("MCO","riego").then(function(items){
-
+meli.search("MCO","playstation").then(function(items){
+    var data = [];
 	items.forEach(function(item){
-
 		var location = item.seller_address;
-
 		if(location.longitude && location.latitude){
-			console.log(location.longitude + "," + location.latitude);
-		}	
-
+		    data.push( {longitude : location.longitude  ,latitude :  location.latitude})
+		}
 	});
+    csv.write(data,{headers: true}).pipe(ws);
 });
 
 
